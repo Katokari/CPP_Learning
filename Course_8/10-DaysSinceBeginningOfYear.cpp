e4d5586e65acc32dcf56a71695d69a73d53e624f
Katokari <47127108+Katokari@users.noreply.github.com>
2025-01-20 17:22:59 +0300

    Solve 11th problem

--- Course_8/10-DaysSinceBeginningOfYear.cpp
+++ Course_8/10-DaysSinceBeginningOfYear.cpp
@@ -20,11 +20,31 @@ int DaysSinceStart(int Day, int Month, int Year) {
     return Days += Day;
 }
 
+std::string DateFromDayOrder(int Order, int Year) {
+    int Month = 1;
+    int Day = 1;
+
+    for (int i = 1; i <= 12; i++) {
+        int MonthDays = NumberOfDaysInMonth(Year, i);
+        if (Order > MonthDays) {
+            Month++;
+            Order -= MonthDays;
+        }
+    }
+
+    Day = Order;
+
+    return std::to_string(Day) + "/" + std::to_string(Month) + "/" + std::to_string(Year);
+}
+
 int main() {
     int Year = input::ReadNumber("Please enter a year? ", "Error, please enter a valid year? ");
     int Month = input::ReadNumber("Please enter a month? ", "Error, please enter a valid month? ");
     int Day = input::ReadNumber("Please enter a day? ", "Error, please enter a valid day? ");
 
-    std::cout << "Days since the beginning of the year is " << DaysSinceStart(Day, Month, Year);
+    int Order = DaysSinceStart(Day, Month, Year);
+    std::cout << "Days since the beginning of the year is " << Order << std::endl;
+
+    std::cout << "Date for [" << Order << "] is: " << DateFromDayOrder(Order, Year);
     return 0;
 }
